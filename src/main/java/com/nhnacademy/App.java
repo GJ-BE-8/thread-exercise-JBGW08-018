e5aceca0b73ec448da4e017d7ba8195c4ba2202e
gimchanghwan <rlackdghks8@naver.com>
2024-09-10 18:50:53 +0900

    exercise2-1

--- src/main/java/com/nhnacademy/App.java
+++ src/main/java/com/nhnacademy/App.java
@@ -56,7 +56,7 @@ public class App
         }
 
         //TODO#10 main Thread는 threadA와 threadB의 상태가 terminated가 될 때 까지 대기 합니다. 즉 threadA, threadB가 종료될 때 까지 대기(양보) 합니다.
-        while(threadA.isAlive() || threadB.isAlive())
+        while(threadA.isAlive() && threadB.isAlive())
         {
             Thread.yield();
         }
