be425d50b0539919802e479ce4957306472e8b31
gimchanghwan <rlackdghks8@naver.com>
2024-09-10 14:10:00 +0900

    exercise1-3

--- src/main/java/com/nhnacademy/thread/CounterHandler.java
+++ src/main/java/com/nhnacademy/thread/CounterHandler.java
@@ -16,14 +16,17 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 //TODO#1 Runnable interface을 implements(구현) 합니다.
-public class CounterHandler {
+public class CounterHandler implements Runnable{
     private final long countMaxSize;
 
     private long count;
 
     public CounterHandler(long countMaxSize) {
         //TODO#2 countMaxSize <=0 이면 IllegalArgumentException()이 발생 합니다.
-
+        if(countMaxSize<=0)
+        {
+            throw new IllegalArgumentException("countMaxSize is less than 0");
+        }
 
         this.countMaxSize = countMaxSize;
         this.count=0l;
@@ -36,9 +39,17 @@ public class CounterHandler {
              - count 1 ~ 10 까지 출력 됩니다.
             ex) thread:my-thread,count:1 ....
          */
-
         do {
-
+            try
+            {
+                Thread.sleep(1000);
+                count++;
+            }
+            catch(InterruptedException e)
+            {
+                log.error("스레드가 중단되었습니다: {}",Thread.currentThread().getName(),e);
+            }
+            log.info("thread: {}, count: {}",Thread.currentThread().getName(),count);
         }while (count<countMaxSize);
     }
 }
